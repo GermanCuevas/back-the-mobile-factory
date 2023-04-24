8f1d858d99bd18b00b347f52cf269fc3220dd57f
German <german.1990.cuevas@gmail.com>
2023-04-24 18:59:46 -0300

    feat: preparando deploy

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
-const db = new Sequelize("mobile-factory-db", null, null, {
-  host: "localhost",
+const db = new Sequelize("dbmobile", "german", "IiGNRk0w0lKfVoyGbsdw5bFE2DXHmcHY", {
+  host: "dpg-ch3f6iks3fvkjtvn3etg-a",
   dialect: "postgres",
   logging: false,
 });
